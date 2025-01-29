fb679492a03ef33c1ec302cb7d0195e5041cb29c
Sergey Golubev <golubev.irk@gmail.com>
2025-01-29 18:21:24 +0100

    initial test

--- mern/server/server.js
+++ mern/server/server.js
@@ -1,6 +1,9 @@
 import express from "express";
 import cors from "cors";
 import records from "./routes/record.js";
+import { dirname } from 'path'
+import path from 'path';
+import { fileURLToPath } from 'url'
 
 const PORT = process.env.PORT || 5050;
 const app = express();
@@ -8,6 +11,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/record", records);
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/dist')));
 
 // start the Express server
 app.listen(PORT, () => {
