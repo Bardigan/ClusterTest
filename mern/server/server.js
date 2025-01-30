8a7999fb0749395a8e746b5e3e7f5e2716bced3e
Sergey Golubev <golubev.irk@gmail.com>
2025-01-30 14:28:21 +0100

    re12

--- mern/server/server.js
+++ mern/server/server.js
@@ -2,12 +2,11 @@ import express from "express";
 import cors from "cors";
 import records from "./routes/record.js";
 import { dirname } from 'path'
-import path from 'path';
+//import path from 'path';
 import { fileURLToPath } from 'url';
 import dotenv from 'dotenv';
 
 dotenv.config();
-
 
 const PORT = process.env.PORT || 8080;
 const app = express();
@@ -20,7 +19,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/dist')));
+//app.use(express.static(path.join(__dirname, 'client/dist')));
 
 // start the Express server
 app.listen(PORT, () => {
