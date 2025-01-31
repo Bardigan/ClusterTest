0a4e177eee25f12e258b47bb0664634936ec76f2
Sergey Golubev <golubev.irk@gmail.com>
2025-01-31 21:17:55 +0100

    36

--- mern/server/server.js
+++ mern/server/server.js
@@ -5,8 +5,6 @@ import { dirname } from 'path'
 import path from 'path';
 import { fileURLToPath } from 'url';
 import dotenv from 'dotenv';
-import fs from 'fs';
-import https from 'https'
 
 dotenv.config();
 
@@ -23,12 +21,7 @@ const __dirname = dirname(__filename);
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'client/dist')));
 
-const sslOptions = {
-  key: fs.readFileSync('path/to/your/ssl/key.pem'),
-  cert: fs.readFileSync('path/to/your/ssl/cert.pem')
-};
-
-// Start the HTTPS server
-https.createServer(sslOptions, app).listen(PORT, () => {
+// start the Express server
+app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
