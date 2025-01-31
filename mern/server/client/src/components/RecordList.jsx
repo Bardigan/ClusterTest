41cfb8adce46237dd293858f78069b7f5ab98739
Sergey Golubev <golubev.irk@gmail.com>
2025-01-31 21:08:17 +0100

    31re

--- mern/server/client/src/components/RecordList.jsx
+++ mern/server/client/src/components/RecordList.jsx
@@ -43,7 +43,7 @@ export default function RecordList() {
   // This method fetches the records from the database.
   useEffect(() => {
     async function getRecords() {
-      const response = await fetch(`https://${API}/record/`);
+      const response = await fetch(`${API}/record/`);
       if (!response.ok) {
         const message = `An error occurred: ${response.statusText}`;
         console.error(message);
@@ -58,7 +58,7 @@ export default function RecordList() {
 
   // This method will delete a record
   async function deleteRecord(id) {
-    await fetch(`https://${API}/record/${id}`, {
+    await fetch(`${API}/record/${id}`, {
       method: "DELETE",
     });
     const newRecords = records.filter((el) => el._id !== id);
